b71533a61e8799c520d9f08a7784cdb50006c760
anjalig21 <anjaligta@outlook.com>
2021-12-31 01:38:40 -0500

    setup pygame complete

---
+++ sort.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+
+display = pygame.display.set_mode((800, 600))
+
+pygame.display.update()
+
+open = True
+
+while open:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            open = False
+
+pygame.quit()
+quit()
