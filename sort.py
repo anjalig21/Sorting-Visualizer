749996cc36a64aaa87c8a4ad743a13a7bcbd7fc8
anjalig21 <anjaligta@outlook.com>
2022-01-10 09:42:33 -0500

    added draw info class and its methods

--- sort.py
+++ sort.py
@@ -1,17 +1,62 @@
 import pygame
-
+import random
 pygame.init()
 
-display = pygame.display.set_mode((800, 600))
+class DrawInfo:
+    BLACK = 0,0,0
+    WHITE = 255,255,255
+    GREEN = 0,255,0
+    RED = 255,0,0
+    GREY = 128,128,128
+    BACKGROUND = WHITE
 
-pygame.display.update()
+    PADDING_SIDES = 100
+    PADDING_TOP = 50
 
-open = True
+    def __init__(self, width, height, lst):
+        self.width = width
+        self.height = height
 
-while open:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            open = False
+        self.window = pygame.display.set_mode((width, height))
+        pygame.display.set_caption("Sorting Algorithm Visualizer")
+        self.set_list(lst)
 
-pygame.quit()
-quit()
+    def set_list(self, lst):
+        self.lst = lst
+        self.min_val = min(lst)
+        self.max_val = max(lst)
+
+        self.block_width = round((self.width - self.PADDING_SIDES) / len(lst)) ## area to represent blocks in / number of blocks
+        self.block_height = round((self.height - self.PADDING_TOP) / (self.max_val - self.min_val)) ## total drawable area / range of values
+        self.start_x = self.PADDING_SIDES // 2 ## integer division by 2 to get whole number
+
+def generate_starting_list(n, min_val, max_val):
+    lst = []
+    for _ in range(n):
+        val = random.randint(min_val, max_val)
+        lst.append(val)
+
+    return lst
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    n = 50
+    min_val = 0
+    max_val = 100
+    lst = generate_starting_list(n, min_val, max_val)
+    draw_info = DrawInfo(800, 600, lst)
+
+    while run:
+        clock.tick(60)
+        pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
+
