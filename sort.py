0c41eb21f843dfcdd64cbf69829b4a2c04a4eb5f
anjalig21 <anjaligta@outlook.com>
2022-01-11 13:30:25 -0500

    controls on display added

--- sort.py
+++ sort.py
@@ -16,8 +16,10 @@ class DrawInfo:
         (192, 192, 192)
     ]
 
+    FONT = pygame.font.SysFont('comicsans', 20)
+    LARGE_FONT = pygame.font.SysFont('comicsans', 40)
     PADDING_SIDES = 100
-    PADDING_TOP = 50
+    PADDING_TOP = 100
 
     def __init__(self, width, height, lst):
         self.width = width
@@ -38,6 +40,13 @@ class DrawInfo:
 
 def draw(draw_info):
     draw_info.window.fill(draw_info.BACKGROUND)
+
+    controls = draw_info.FONT.render("R - Reset | SPACE - Start Sorting | A - Ascending | D - Descending", 1, draw_info.BLACK)
+    draw_info.window.blit(controls, (draw_info.width/2 - controls.get_width()/2,5)) ## put controls in center of screen
+
+    sorting = draw_info.FONT.render("I - Insertion Sort | B - Bubble Sort", 1, draw_info.BLACK)
+    draw_info.window.blit(sorting, (draw_info.width/2 - sorting.get_width()/2,35)) ## put controls in center of screen
+
     draw_list(draw_info)
     pygame.display.update()
 
@@ -68,6 +77,8 @@ def main():
     max_val = 100
     lst = generate_starting_list(n, min_val, max_val)
     draw_info = DrawInfo(800, 600, lst)
+    sorting = False
+    ascending = True
 
     while run:
         clock.tick(60)
@@ -81,6 +92,13 @@ def main():
             if event.key == pygame.K_r:
                 lst = generate_starting_list(n, min_val, max_val)
                 draw_info.set_list(lst)
+                sorting = False
+            elif event.key == pygame.K_SPACE and sorting == False:
+                sorting = True
+            elif event.key == pygame.K_a and not sorting:
+                ascending = True
+            elif event.key == pygame.K_d and not sorting:
+                ascending = False
 
     pygame.quit()
 
